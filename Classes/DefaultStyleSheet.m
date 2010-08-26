3d2d09f48a6abd50cc627c6d00b244ba2602f1f9
PEZ <pez@pezius.com>
2010-08-26 23:58:14 +0200

    Styled the figure counter (badge)

--- Classes/DefaultStyleSheet.m
+++ Classes/DefaultStyleSheet.m
@@ -15,4 +15,24 @@
   return RGBCOLOR(11, 11, 11);
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (TTStyle*)badgeWithFontSize:(CGFloat)fontSize {
+  return
+  [TTShapeStyle styleWithShape:[TTRoundedRectangleShape shapeWithRadius:3] next:
+   [TTInsetStyle styleWithInset:UIEdgeInsetsMake(1, 1, 1, 1) next:
+    [TTShadowStyle styleWithColor:RGBACOLOR(0,0,0,0.8) blur:3 offset:CGSizeMake(0, 4) next:
+     [TTReflectiveFillStyle styleWithColor:RGBACOLOR(12,140,21,0.9) next:
+      [TTInsetStyle styleWithInset:UIEdgeInsetsMake(-1, -1, -1, -1) next:
+       [TTSolidBorderStyle styleWithColor:[UIColor whiteColor] width:2 next:
+        [TTBoxStyle styleWithPadding:UIEdgeInsetsMake(1, 7, 2, 7) next:
+         [TTTextStyle styleWithFont:[UIFont boldSystemFontOfSize:fontSize]
+                              color:[UIColor whiteColor] next:nil]]]]]]]];
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (TTStyle*)largeBadge {
+  return [self badgeWithFontSize:14];
+}
+
+
 @end
